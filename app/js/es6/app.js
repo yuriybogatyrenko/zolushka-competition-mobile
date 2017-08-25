734a4876388f573d10d48306e4fb35fb5a7a061e
geeekteam <stanislavrylsk@gmail.com>
2017-08-25 19:47:35 +0600

    done

--- app/js/es6/app.js
+++ app/js/es6/app.js
@@ -547,16 +547,14 @@ class YOURAPPNAME {
                 enabled: true
             }
         });
-        });
-
-        $('[data-scroll-down]').on('click', function(e) {
-            e.preventDefault();
-
-            const scrollTop = parseInt($('.main-first-screen').offset().top) + parseInt($('.main-first-screen ').outerHeight());
-            console.log(scrollTop);
-
-            $('body, html').animate({scrollTop: scrollTop}, 800);
-        });
     });
 
+    $('[data-scroll-down]').on('click', function (e) {
+        e.preventDefault();
+
+        const scrollTop = parseInt($('.main-first-screen').offset().top) + parseInt($('.main-first-screen ').outerHeight());
+        console.log(scrollTop);
+
+        $('body, html').animate({scrollTop: scrollTop}, 800);
+    });
 })();
